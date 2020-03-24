ff4ae565378a1db410c163df70b95060e15f374e
Hikari Sakamoto <hikari.sakamoto@dbserver.com.br>
2020-03-24 01:32:04 -0300

    Wrapping up

--- worker/worker.py
+++ worker/worker.py
@@ -1,10 +1,12 @@
 import redis
 import json
+import os
 from time import sleep
 from random import randint
 
 if __name__ == '__main__':
-    rds = redis.Redis(host='queue', port=6379, db=0)
+    redis_host = os.getenv('REDIS_HOST', 'queue')
+    rds = redis.Redis(host=redis_host, port=6379, db=0)
     print('Aguardando mensagens...')
     while True:
         mensagem = json.loads(rds=blpop('sender') [1])
